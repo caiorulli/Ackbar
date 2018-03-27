eca957e79fd3fed620a2b22abaf63d714bcfc0c4
Caio <caiorulli@gmail.com>
2018-03-27 00:26:41 -0300

    Adding starter tutorial classes

---
+++ Ackbar/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ackbar.Models;
+
+namespace Ackbar.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+
+            if (_context.Users.Count() == 0)
+            {
+                _context.Users.Add(new User { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
