94d522f852bdd7d03cdfda656ab6dde09faa917f
Tiago Deliberali <tiago.deliberali@gmail.com>
2018-04-20 01:55:10 +0000

    added sql server as docker compose

---
+++ Ackbar/Controllers/Admin/MigrationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ackbar.Controllers.Admin
+{
+    public class MigrationController : Controller
+    {
+        private GameGuideContext db;
+
+        public MigrationController(GameGuideContext dbContext)
+        {
+            this.db = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                db.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View();
+        }
+    }
+}
