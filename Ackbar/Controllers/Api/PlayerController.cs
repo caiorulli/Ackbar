110668704a4870b8bf7ddaa30ae8d3ce96c8075d
Caio Rulli <caiorulli@gmail.com>
2018-05-17 00:14:42 -0300

    Added random recommendation endpoint

--- Ackbar/Controllers/Api/PlayerController.cs
+++ Ackbar/Controllers/Api/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using Ackbar.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ackbar.Controllers.Api
 {
@@ -44,6 +45,34 @@ namespace Ackbar.Controllers.Api
             _context.Likes.Add(like);
             _context.SaveChanges();
             return Ok();
+        }
+
+        [HttpGet("Recommendations")]
+        public IActionResult Recommendations()
+        {
+            var currentUser = HttpContext.User;
+            if (!currentUser.HasClaim(c => c.Type == ClaimTypes.NameIdentifier))
+            {
+                return Unauthorized();
+            }
+            var userId = long.Parse(currentUser.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+            var recommendedGames = _context.Games
+                .FromSql("SELECT * from Games " +
+                         " WHERE Id NOT IN (SELECT g.Id FROM Games g " +
+                         "INNER JOIN Likes l ON g.Id = l.GameId " +
+                         "INNER JOIN Players p ON l.PlayerId = p.Id " +
+                         "INNER JOIN Users u ON u.Id = p.UserId " +
+                         "WHERE u.Id = {0})", userId)
+                .ToArray();
+
+            var recommendations = new long[recommendedGames.Length];
+            for (var i = 0; i < recommendedGames.Length; i++)
+            {
+                recommendations[i] = recommendedGames[i].Id;
+            }
+
+            return Ok(recommendations);
         }
     }
 }
